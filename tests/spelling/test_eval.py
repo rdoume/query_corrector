6df26c7077004c83c079259c8d8d168908c0f992
Robin Doumerc <Robin@doumerc.eu>
2018-04-13 16:01:52 +0200

    Add unit tests for performance evaluation

---
+++ tests/spelling/test_eval.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+from ccquery.spelling import Evaluation
+from ccquery.utils import io_utils
+
+class TestEvalSpelling(unittest.TestCase):
+    """Test the performance evaluation of automation corrections"""
+
+    def setUp(self):
+        """Set up local variables"""
+        self.cfile = os.path.join(
+            os.path.dirname(__file__), 'sample-queries.jsonl')
+
+    def test_do_nothing(self):
+        """Test the performance of a 'do nothing' algorithm"""
+
+        evaluation = Evaluation()
+
+        for n in [10, 5, 1]:
+            evaluation.load_from_file(self.cfile, 'noisy', 'clean')
+            scores = evaluation.performance(n)
+            self.assertEqual(scores, (83.33, 83.33, 83.33))
+
+    def test_perfect_correction(self):
+        """Test the performance of a 'perfect correction' algorithm"""
+
+        evaluation = Evaluation()
+        evaluation.load_from_file(self.cfile, 'clean', 'clean')
+
+        scores = evaluation.performance(1)
+        self.assertEqual(scores, (100.00, 100.00, 100.00))
