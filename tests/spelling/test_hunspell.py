1e5e06d229787fba0f27d07e1c70ec4d6a3b6c29
Robin Doumerc <Robin@doumerc.eu>
2018-04-25 16:11:21 +0200

    Complete unit tests on hunspelling

--- tests/spelling/test_hunspell.py
+++ tests/spelling/test_hunspell.py
@@ -17,24 +17,44 @@ class TestHunSpelling(unittest.TestCase):
             os.path.dirname(__file__), 'sample-queries.jsonl')
 
         io_utils.check_file_readable(self.aff)
+        io_utils.check_file_readable(self.dic)
         io_utils.check_file_readable(self.samples)
 
-        # prepare index.dic
-        # use the wiki test dictionary and add the header linecount
-        wiki_dict = os.path.join(
-            os.path.dirname(__file__), '..', 'wiki', 'sample-vocab.txt')
+    def test_extra_load(self):
+        """Add an extra dictionary"""
 
-        n = io_utils.count_lines(wiki_dict)
-        with open(self.dic, 'w', encoding='utf-8') as ostream:
-            ostream.write(str(n) + '\n')
-            with open(wiki_dict, 'r', encoding='utf-8') as istream:
-                for line in istream:
-                    ostream.write(line.strip() + '\n')
+        spell_checker = HunSpelling(self.dic, self.aff, self.dic)
+        self.assertFalse(None, spell_checker)
 
-    def tearDown(self):
-        """Remove temporary file"""
-        if os.path.exists(self.dic):
-            os.remove(self.dic)
+    def test_spellings(self):
+        """Check spellings"""
+
+        spell_checker = HunSpelling(self.dic, self.aff)
+        words = ['aide', 'brut', 'pourquoi', 'calcul', 'ville']
+        errors = [spell_checker.is_misspelled(w) for w in words]
+        self.assertEqual([False, False, True, False, True], errors)
+
+        # incorrectly test adding new words
+        spell_checker.add_words(None)
+
+        # add new words and recheck spelling
+        nwords = ['pourquoi', 'ville']
+        spell_checker.add_words(nwords)
+        errors = [spell_checker.is_misspelled(w) for w in nwords]
+        self.assertEqual([False, False], errors)
+
+        # remove the newly added words and recheck spelling
+        spell_checker.remove_words(None)
+        spell_checker.remove_words(nwords)
+        errors = [spell_checker.is_misspelled(w) for w in nwords]
+        self.assertEqual([True, True], errors)
+
+    def test_new_dict(self):
+        """Test adding an extra dictionary"""
+
+        spell_checker = HunSpelling(self.dic, self.aff)
+        spell_checker.add_extra_dictionary(self.dic)
+        self.assertFalse(None, spell_checker)
 
     def test_suggestions(self):
         """Test the automatic spelling corrections"""
